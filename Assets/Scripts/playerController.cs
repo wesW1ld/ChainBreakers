d210ca2e8dd5902ff2cb82b1f91316d9b2bb0f65
Wesley Wild <wdw0510@gmail.com>
2025-09-26 18:11:17 -0400

    Basic Input and Movement

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
+    public float moveSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 direction;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+    }
+
+    void OnMove(InputValue value)
+    {
+        direction = value.Get<Vector2>();
     }
 }
