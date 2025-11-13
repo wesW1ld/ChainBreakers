36b09669a8e53297718d74bffb99b4ee363dc474
Wesley Wild <wdw0510@gmail.com>
2025-11-13 15:13:57 -0500

    Played cards stack

--- Assets/Scripts/PlayList.cs
+++ Assets/Scripts/PlayList.cs
@@ -5,20 +5,72 @@ using UnityEngine;
 
 public class PlayList : MonoBehaviour
 {
-    //add a int for the play card limit 
-    List <Sprite> cardPic = new List<Sprite>();
-    List <Card> playedCards = new List<Card>();
+    Stack<Sprite> CardPics = new Stack<Sprite>();
+    Stack<Card> PlayedCards = new Stack<Card>();
+    public int CardLimit = 10;
 
-
-    // Start is called before the first frame update
-    void Start()
+    public void Push()
     {
-        //show an empty list of played cards at the start, for the limit 
+        if(PlayedCards.Count == CardLimit)
+        {
+            Debug.Log("Max number of cards played");
+            return;
+        }
+        CardPics.Push(Sprite);
+        PlayedCards.Push(Card);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Pop()
     {
-        
+        CardPics.Pop();
+        PlayedCards.Pop();
     }
+
+    public Card[] SeeStack() //see current stack with top being index 0
+    {
+        Card[] ret = PlayedCards.ToArray();
+        return ret;
+    }
+
+    public Card Top()
+    {
+        return PlayedCards.Peek();
+    }
+
+    private void UpdateDisplay()
+    {
+
+    }
+    
+    //singleton stuff
+    public static PlayList instance;
+    public static PlayList Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("GameManager is Null");
+            }
+            return instance;
+        }
+
+    }
+    private void Awake()
+    {
+
+        if (instance)
+        {
+            Debug.LogError("GameManager is already in the scene");
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            Debug.Log("playermanager awakened");
+            //DontDestroyOnLoad(this); //stay over scene
+        }
+
+    }
+    
 }
