7f117fccc75cc5e7e683f3b4b2d338eb782165c3
Kai Volsteedt <kaiaidanv@gmail.com>
2025-11-13 14:53:49 -0500

    Started the drag ui

---
+++ Assets/Scripts/PlayArea.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using ChainBreakers;
+using UnityEngine;
+
+public class PlayArea : MonoBehaviour
+{
+    Card card;
+    Sprite playedCard;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the local position of the card, if the card is within the play area, make a small sprite of the card into the playList
+    }
+}
